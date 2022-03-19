21cde5e3d83ae79c2300a2a8e1e81f9fa559a82b
komalshruti <shrutikomal30@gmail.com>
2022-03-19 19:00:49 +0530

    End of Section2

---
+++ student-adminportal-ui/src/app/students/students.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
+import { StudentService } from '../student.service';
+
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  students: Student[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender', 'edit'];
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+
+
+
+  constructor(private studentService: StudentService) {
+  }
+
+  ngOnInit(): void {
+    this.studentService.getStudents()
+    .subscribe(
+      (successResponse)=>{
+        this.students = successResponse;
+        this.dataSource = new MatTableDataSource<Student>(this.students);
+      },
+      (errorResponse) =>{
+        console.log(errorResponse);
+      }
+
+
+    );
+
+  }
+
+}
